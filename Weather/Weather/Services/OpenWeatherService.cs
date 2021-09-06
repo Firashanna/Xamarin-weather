466965b13a47bf00db6a98c0ea1984f03aedf0d2
Firashanna <56591432+Firashanna@users.noreply.github.com>
2021-09-06 11:46:40 +0200

    Add project files.

---
+++ Weather/Weather/Services/OpenWeatherService.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Json; //Requires nuget package System.Net.Http.Json
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+using Weather.Models;
+
+namespace Weather.Services
+{
+    //You replace this class witth your own Service from Project Part A
+    public class OpenWeatherService
+    {
+        HttpClient httpClient = new HttpClient();
+        readonly string apiKey = "a0311119d3f08ce24bcd33ce20fbdf03"; // Your API Key
+
+        //part of your event code here
+        public async Task<Forecast> GetForecastAsync(string City)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?q={City}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            OnWeatherForecastAvailable("New Weather forecast for " + City + " available");
+
+            return forecast;
+
+        }
+        public async Task<Forecast> GetForecastAsync(double latitude, double longitude)
+        {
+            //https://openweathermap.org/current
+            var language = System.Globalization.CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+            var uri = $"https://api.openweathermap.org/data/2.5/forecast?lat={latitude}&lon={longitude}&units=metric&lang={language}&appid={apiKey}";
+
+            Forecast forecast = await ReadWebApiAsync(uri);
+
+            OnWeatherForecastAvailable("New Weather forecast for (" + latitude.ToString() + " , " + longitude.ToString() + ") available"); ;
+
+            return forecast;
+        }
+        private async Task<Forecast> ReadWebApiAsync(string uri)
+        {
+            using (HttpResponseMessage response = await httpClient.GetAsync(uri))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    Forecast f = new Forecast();
+                    WeatherApiData o = await response.Content.ReadFromJsonAsync<WeatherApiData>();
+                    f.City = o.city.name;
+                    f.Items = o.list.Select(x => new ForecastItem()
+                    {
+                        Temperature = x.main.temp,
+                        DateTime = Convert.ToDateTime(x.dt_txt),
+                        Description = x.weather[0].description
+                    ,
+                        Icon = x.weather[0].icon,
+                        WindSpeed = x.wind.speed
+                    }).ToList();
+                    return f;
+
+
+
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+            return null;
+        }
+        private DateTime UnixTimeStampToDateTime(double unixTimeStamp)
+        {
+            DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+            dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
+            return dateTime;
+        }
+
+        public event EventHandler<string> WeatherForecastAvailable;
+
+
+        protected virtual void OnWeatherForecastAvailable(string message)
+        {
+            EventHandler<String> handler = WeatherForecastAvailable;
+            if (handler != null)
+            {
+                handler(this, message);
+            }
+        }
+    }
+}
---
+++ Weather/Weather/Views/ForecastPage.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Weather.Models;
+using Weather.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Weather.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class ForecastPage : ContentPage
+    {
+        OpenWeatherService service;
+        GroupedForecast groupedforecast;
+        string name="";
+        public ForecastPage()
+        {
+            InitializeComponent();
+            service = new OpenWeatherService();
+            groupedforecast = new GroupedForecast();
+        
+        }
+
+        protected override void OnAppearing()
+        {
+            name = this.Title;
+            base.OnAppearing();
+            this.Title = "Forecast of " + name;
+            MainThread.BeginInvokeOnMainThread(async () => { await LoadForecast(); });
+
+        }
+
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            groupedforecast = new GroupedForecast();
+            MainThread.BeginInvokeOnMainThread(async () => { await LoadForecast(); });
+        }
+        private async Task LoadForecast()
+        {
+
+            Task<Forecast> t = service.GetForecastAsync(name);
+            var a = await t;
+
+            if (t.Status == TaskStatus.RanToCompletion)
+            {
+                Forecast forecast = a;
+                groupedforecast.City = a.City;
+
+                groupedforecast.Items = a.Items.GroupBy(x => x.DateTime.Date);
+
+
+
+
+            }
+            DataTemplate i = new DataTemplate();
+
+            output.ItemsSource = groupedforecast.Items;
+            output.IsGroupingEnabled = true;
+            output.GroupDisplayBinding = new Binding("Key");
+            output.GroupHeaderTemplate = new DataTemplate(typeof(HeaderCell));
+            output.ItemTemplate = new DataTemplate(typeof(ItemCell));
+
+        }
+    }
+
+
+    public class HeaderCell : ViewCell
+    {
+        public HeaderCell()
+        {
+
+            var title = new Label
+            {
+                TextColor = Color.Blue,
+                Font = Font.SystemFontOfSize(NamedSize.Small, FontAttributes.Bold)
+
+            };
+
+            title.SetBinding(Label.TextProperty, "Key", converter: new DateFormatter());
+            string s = title.Text;
+            View = new StackLayout
+            {
+                HeightRequest = 25,
+                Children = { title }
+            };
+        }
+    }
+
+    public class ItemCell : ViewCell
+    {
+        public ItemCell()
+        {
+            var a = this.GetValue(MenuProperty);
+            var Time = new Label
+            {
+
+                Font = Font.SystemFontOfSize(NamedSize.Small)
+
+            };
+
+            var item = new Label();
+            
+
+            Time.SetBinding(Label.TextProperty, "DateTime", converter: new TimeFormatter());
+            item.SetBinding(Label.TextProperty, new MultiBinding
+            {
+                Bindings = new Collection<BindingBase>
+            {
+                new Binding("Description"),
+                new Binding("Temperature"),
+                new Binding("WindSpeed"),
+            },
+                Converter = new AllMultiConverter()
+            });
+            View = new StackLayout
+            {
+
+                Children = { Time, item }
+            };
+        }
+    }
+
+
+
+    public class DateFormatter : IValueConverter
+    {
+    
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime temp = (DateTime)value;
+            return temp.ToString("dddd,  MMMM dd, yyyy");
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class TimeFormatter : IValueConverter
+    {
+     
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime temp = (DateTime)value;
+            return temp.ToString("h:mm tt");
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class AllMultiConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"{values[0]}, temperature: {values[1]} degC, wind: {values[2]} m/s";
+         
+        }
+        //Detta ska inte omplementras pga inte används..
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+          
+            throw new NotImplementedException();
+        }
+    }
+
+}
+
